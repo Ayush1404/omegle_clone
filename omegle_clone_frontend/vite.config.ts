cc555c41b5ff0314864592b8312ddfdfd7ce0c27
Ayush <ayushkadam1404.com>
2024-02-15 21:33:01 +0530

    removing errors for deployement

--- omegle_clone_frontend/vite.config.ts
+++ omegle_clone_frontend/vite.config.ts
@@ -5,10 +5,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: './key.pem',
-      cert: './cert.pem',
-    },
-  },
 });
