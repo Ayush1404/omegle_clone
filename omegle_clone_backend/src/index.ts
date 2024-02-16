e244899eb6479d5a5fbc10a005414127163fc698
Ayush <ayushkadam1404.com>
2024-02-16 20:11:16 +0530

    fixing error for deployment

--- omegle_clone_backend/src/index.ts
+++ omegle_clone_backend/src/index.ts
@@ -1,33 +1,29 @@
 import express from 'express';
-import { createServer } from 'node:http';
+import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { UserManager } from './managers/userManager';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server ,{
-  cors:{
-    origin:"*"
-  }
-});
+const io = new Server(server);
+
 const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-  res.send("server is runnning")
+  res.send("server is running");
 });
 
-const userManager =new UserManager();
+const userManager = new UserManager();
 
 io.on('connection', (socket) => {
   console.log(`a user connected ${socket.handshake.query.name}`);
-  const name = socket.handshake.query.name
-  if(!name || Array.isArray(name)) return 
-  userManager.addUser(name,socket)
-  socket.on('disconnect',()=>{
-    console.log("userdiconncted")
-    userManager.removeUser(socket.id)
-  })
+  const name = socket.handshake.query.name;
+  if (!name || Array.isArray(name)) return;
+  userManager.addUser(name, socket);
+  socket.on('disconnect', () => {
+    console.log("user disconnected");
+    userManager.removeUser(socket.id);
+  });
 });
 
-server.listen(port, () => {
-  console.log('server running at http://localhost:3000');
-});
+export default server;
